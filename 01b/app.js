e6af06f3b2f59621686b64e212654afa8f071059
Asta <omaralinora.1@gmail.com>
2023-11-21 12:26:46 +0100

    ok

---
+++ 01b/app.js
@@ -0,0 +1,26 @@
+class App {
+    runApplication() {
+       
+        console.log("hallo school!"); 
+        // Overige code hier
+        this.greeting ="starting up"
+        let appNaam = "les1"; 
+        let versienummer = 0.9;
+        let versiedatum = new Date(21/11/2023); 
+        let auteur = "omar kahouach";
+        let copyright = "media college "; 
+        let distributeur = "visualcode"; 
+        let darkmode = false;
+}
+}
+
+let app = new App();
+app.runApplication();
+        
+console.log("appNaam:", appNaam);
+        console.log("Versienummer:", versienummer);
+        console.log("Versiedatum:", versiedatum);
+        console.log("Auteur:", auteur);
+        console.log("Copyright:", copyright);
+        console.log("Distributeur:", distributeur);
+        console.log("Darkmode:", darkmode);
