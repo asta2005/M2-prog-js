5a3f7dc31986f59d7f6322f4b148e469f30ccf67
asta2005 <144676646+asta2005@users.noreply.github.com>
2023-11-28 12:15:44 +0100

    Add files via upload

---
+++ ifelse/app.js
@@ -0,0 +1,13 @@
+class App {
+    runApplication() 
+    {
+        //hier komt je code 
+        //dit is code commentaar
+        //dit is ook commentaar
+       
+       
+    }
+}
+
+let app = new App();
+app.runApplication();
